97975458a3be4a11d37f168a2185f8b2674fddc7
zzz_xxj <995007786@qq.com>
2019-05-06 13:51:57 +0800

    添加MDK编译例程（复制F767相同例程）

---
+++ MDK编译过程及文件全解/MDK文件详解-GPIO输出—多彩流水灯/User/main.c
@@ -0,0 +1,155 @@
+/**
+  ******************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2018-xx-xx
+  * @brief   GPIO输出--使用固件库点亮LED灯
+  ******************************************************************
+  * @attention
+  *
+  * 实验平台:野火 STM32H743开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://firestm32.taobao.com
+  *
+  ******************************************************************
+  */  
+#include "stm32h7xx.h"
+#include "main.h"
+#include "./led/bsp_led.h"
+#include "./delay/core_delay.h"   
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void)
+{
+	/* 系统时钟初始化成400MHz */
+	SystemClock_Config();
+	/* LED 端口初始化 */
+	LED_GPIO_Config();	
+	/* 控制LED灯 */
+	while (1)
+	{
+		LED1( ON );			 // 亮 
+		HAL_Delay(1000);
+		LED1( OFF );		  // 灭
+		HAL_Delay(1000);
+
+		LED2( ON );			// 亮 
+		HAL_Delay(1000);
+		LED2( OFF );		  // 灭
+
+		LED3( ON );			 // 亮 
+		HAL_Delay(1000);
+		LED3( OFF );		  // 灭	
+		
+		/*轮流显示 红绿蓝黄紫青白 颜色*/
+		LED_RED;
+		HAL_Delay(1000);
+		
+		LED_GREEN;
+		HAL_Delay(1000);
+		
+		LED_BLUE;
+		HAL_Delay(1000);
+		
+		LED_YELLOW;
+		HAL_Delay(1000);
+		
+		LED_PURPLE;
+		HAL_Delay(1000);
+						
+		LED_CYAN;
+		HAL_Delay(1000);
+		
+		LED_WHITE;
+		HAL_Delay(1000);
+		
+		LED_RGBOFF;
+		HAL_Delay(1000);
+	}
+}
+
+/**
+  * @brief  System Clock 配置
+  *         system Clock 配置如下: 
+	*            System Clock source  = PLL (HSE)
+	*            SYSCLK(Hz)           = 400000000 (CPU Clock)
+	*            HCLK(Hz)             = 200000000 (AXI and AHBs Clock)
+	*            AHB Prescaler        = 2
+	*            D1 APB3 Prescaler    = 2 (APB3 Clock  100MHz)
+	*            D2 APB1 Prescaler    = 2 (APB1 Clock  100MHz)
+	*            D2 APB2 Prescaler    = 2 (APB2 Clock  100MHz)
+	*            D3 APB4 Prescaler    = 2 (APB4 Clock  100MHz)
+	*            HSE Frequency(Hz)    = 25000000
+	*            PLL_M                = 5
+	*            PLL_N                = 160
+	*            PLL_P                = 2
+	*            PLL_Q                = 4
+	*            PLL_R                = 2
+	*            VDD(V)               = 3.3
+	*            Flash Latency(WS)    = 4
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  HAL_StatusTypeDef ret = HAL_OK;
+  
+  /*使能供电配置更新 */
+  MODIFY_REG(PWR->CR3, PWR_CR3_SCUEN, 0);
+
+  /* 当器件的时钟频率低于最大系统频率时，电压调节可以优化功耗，
+		 关于系统频率的电压调节值的更新可以参考产品数据手册。  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+ 
+  /* 启用HSE振荡器并使用HSE作为源激活PLL */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+  RCC_OscInitStruct.CSIState = RCC_CSI_OFF;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+
+  RCC_OscInitStruct.PLL.PLLM = 5;
+  RCC_OscInitStruct.PLL.PLLN = 160;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+ 
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
+  ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
+  if(ret != HAL_OK)
+  {
+
+    while(1) { ; }
+  }
+  
+	/* 选择PLL作为系统时钟源并配置总线时钟分频器 */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK  | \
+																 RCC_CLOCKTYPE_HCLK    | \
+																 RCC_CLOCKTYPE_D1PCLK1 | \
+																 RCC_CLOCKTYPE_PCLK1   | \
+                                 RCC_CLOCKTYPE_PCLK2   | \
+																 RCC_CLOCKTYPE_D3PCLK1);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;  
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2; 
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2; 
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2; 
+  ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4);
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }
+}
+/****************************END OF FILE***************************/
