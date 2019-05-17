035100eadd848c71f8b2c9e42118ea98fb78f637
zzz_xxj <995007786@qq.com>
2019-05-17 16:01:13 +0800

    上位机例程卡在main函数一开始的地方

---
+++ MPU6050（包含程序+上位机）/程序/3.STM32-MPU6050_python上位机/User/main.h
@@ -0,0 +1,14 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32h7xx_hal.h"
+
+static void SystemClock_Config(void);
+/*简单任务管理*/
+//#define TASK_ENABLE 0
+//#define NumOfTask 3
+//extern uint32_t Task_Delay[NumOfTask];
+
+//static void SystemClock_Config(void);
+#endif /* __MAIN_H */
+
